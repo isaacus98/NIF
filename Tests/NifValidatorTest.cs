20adb18c8c54a2955402472460fc742ef79a5d14
Isaac Morcillo Garcia <isaacus98@gmail.com>
2022-07-02 18:08:16 +0200

    Tests

--- Tests/NifValidatorTest.cs
+++ Tests/NifValidatorTest.cs
@@ -1,11 +1,56 @@
+using NIF;
+
 namespace Tests
 {
     [TestClass]
     public class NifValidatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidateNIFTestTrue()
         {
+            NifValidator validator = new NifValidator();
+            bool result = validator.Validate("32700667A");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateNIFTestFalse()
+        {
+            NifValidator validator = new NifValidator();
+            bool result = validator.Validate("32700667B");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateNIETestTrue()
+        {
+            NifValidator validator = new NifValidator();
+            bool result = validator.Validate("Y2910565K");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateNIETestFalse()
+        {
+            NifValidator validator = new NifValidator();
+            bool result = validator.Validate("Y2910565Q");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateCIFTestTrue()
+        {
+            NifValidator validator = new NifValidator();
+            bool result = validator.Validate("A21109525");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateCIFTestFalse()
+        {
+            NifValidator validator = new NifValidator();
+            bool result = validator.Validate("Q21109526");
+            Assert.IsFalse(result);
         }
     }
 }
