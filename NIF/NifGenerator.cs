90e76183e0eb822f3ac128533ccd0a5f4a798847
Isaac Morcillo Garcia <isaacus98@gmail.com>
2022-07-03 00:49:19 +0200

    Creation helpers region and CalculateLetter method

--- NIF/NifGenerator.cs
+++ NIF/NifGenerator.cs
@@ -12,5 +12,15 @@ namespace NIF
         {
 
         }
+
+        #region "Helpers"
+
+        private String CalculateLetter(int number)
+        {
+            String[] letters = new string[] { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E" };
+            return letters[number % 23];
+        }
+
+        #endregion
     }
 }
