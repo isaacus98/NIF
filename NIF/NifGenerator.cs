056aa75a0cd54bcb9fa66f7f6796d7d0ea2ac29f
Isaac Morcillo Garcia <isaacus98@gmail.com>
2022-07-18 20:34:48 +0200

    Method GenerateCif

--- NIF/NifGenerator.cs
+++ NIF/NifGenerator.cs
@@ -40,6 +40,47 @@ namespace NIF
 
         }
 
+        private String CalculateControlDigit(String cif)
+        {
+            string letterCif;
+            string xxx;
+            int addition;
+            int counter;
+            int controlDigit;
+            int evenNumber = 0;
+            int oddNumber = 0;
+            string[] letter = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "J", "N", "P", "Q", "R", "S", "U", "V", "W" };
+
+            letterCif = cif.Substring(0, 1);
+
+            for (counter = 1; counter < 7; counter++)
+            {
+                xxx = (2 * int.Parse(cif.Substring(counter++, 1))) + "0";
+                oddNumber += int.Parse(xxx.ToString().Substring(0, 1)) + int.Parse(xxx.ToString().Substring(1, 1));
+                evenNumber += int.Parse(cif.Substring(counter, 1));
+            }
+
+            xxx = (2 * int.Parse(cif.Substring(counter, 1))) + "0";
+            oddNumber += int.Parse(xxx.Substring(0, 1)) + int.Parse(xxx.Substring(1, 1));
+
+            addition = evenNumber + oddNumber;
+
+            controlDigit = int.Parse(addition.ToString().Substring(addition.ToString().Length - 1, 1));
+            controlDigit = 10 - controlDigit;
+
+            if (controlDigit == 10)
+                controlDigit = 0;
+
+            if (letterCif == "P" || letterCif == "Q" || letterCif == "R" || letterCif == "S" || letterCif == "W" || letterCif == "N")
+            {
+                return letter[controlDigit - 1];
+            }
+            else
+            {
+                return controlDigit.ToString();
+            }
+        }
+
         #endregion
 
         public String GenerateDNI()
@@ -121,6 +162,34 @@ namespace NIF
             }
 
             return nie;
+        }
+
+        public String GenerateCIF()
+        {
+            Random random = new Random();
+            //string[] letterDigitControl = new string[] { "J", "A", "B", "C", "D", "E", "F", "G", "H", "I" };
+            string[] letter = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "J", "N", "P", "Q", "R", "S", "U", "V", "W" };
+            string[] regionCode = new string[] { "01", "02", "03", "53", "54", "04", "05", "06", "07", "57", "08", "58", "59", "60", "61", "62", "63", "64", "09", "10", "11", "72", "12", "13",
+                                                "14", "56", "15", "70", "16", "17", "55", "18", "19", "20", "71", "21", "22", "23", "24", "25", "26", "27", "28", "78", "79", "80", "81", "82",
+                                                "83", "84", "29", "92", "93", "30", "73", "31", "32", "33", "74", "34", "35", "76", "36", "94", "37", "38", "75", "39", "40", "41", "91", "42",
+                                                "43", "77", "44", "45", "46", "96", "97", "98", "47", "48", "95", "49", "50", "99", "51", "52"};
+            string cif = "";
+
+            //Get Random letter
+            cif += letter[random.Next(0, letter.Length)];
+            //Get random region code
+            cif += regionCode[random.Next(0, regionCode.Length)];
+
+            //Generate 5 random numbers from 0 to 9
+            for (int i = 0; i < 5; i++)
+                cif += random.Next(0, 9).ToString();
+
+
+            //Get control digit
+            cif += CalculateControlDigit(cif);
+
+
+            return cif;
         }
     }
 }
