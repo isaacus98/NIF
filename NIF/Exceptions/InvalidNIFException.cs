e05a5e1124b240bf6d8660c3bef9a02ed0585b0c
Isaac Morcillo <isaacus98@gmail.com>
2025-02-15 16:26:24 +0100

    Excepción personalizada InvalidNIFException creada

---
+++ NIF/Exceptions/InvalidNIFException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace NIF.Exceptions
+{
+    internal class InvalidNIFException : Exception
+    {
+        public InvalidNIFException()
+        {
+        }
+
+        public InvalidNIFException(string message) : base(message)
+        {
+        }
+
+        public InvalidNIFException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidNIFException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
